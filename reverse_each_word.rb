7bdd375ea026640c47a42594cce4cbc2e470fde9
Matt Bechtel <bechma02@gmail.com>
2019-10-22 03:58:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,8 @@
 def reverse_each_word(string)
   string_array = string.split
-  string_array.each { |word|
-    word.reverse
+  backwards_array = string_array.map do |word|
+    '#{word.reverse}'
+  end
   }
 
 end
